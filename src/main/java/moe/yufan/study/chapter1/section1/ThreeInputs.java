37c08f6ef22f8db7fba83d0eb4801b752d3d3cc3
syhily <syhily@gmail.com>
2020-12-04 21:06:17 +0800

    chore: finish question 1.1.3

---
+++ src/main/java/moe/yufan/study/chapter1/section1/ThreeInputs.java
@@ -0,0 +1,40 @@
+package moe.yufan.study.chapter1.section1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Write a program that takes three integer command-line arguments and prints equal if all three are equal,
+ * and not equal otherwise.
+ *
+ * @author せいうはん (Wesleysheng)
+ * @version 1.0.0, 2020-12-04 20:54
+ * @since 1.0.0, 2020-12-04 20:54
+ */
+public class ThreeInputs {
+
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            StdOut.println("You should input three arguments");
+            System.exit(-1);
+        }
+
+        // Parse args.
+        int[] arguments = new int[3];
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            try {
+                int argument = Integer.parseInt(arg);
+                arguments[i] = argument;
+            } catch (NumberFormatException e) {
+                StdOut.printf("This argument %s is not a integer\n", arg);
+                System.exit(-1);
+            }
+        }
+
+        if (arguments[0] == arguments[1] && arguments[1] == arguments[2]) {
+            StdOut.println("equal");
+        } else {
+            StdOut.println("not equal");
+        }
+    }
+}
---
+++ src/main/java/moe/yufan/study/package-info.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright © 2019-2020 Tencent Co., Ltd.
+ *
+ * This file is part of benchmark project.
+ * Do not copy, cite, or distribute without the express
+ * permission from Cloud Monitor group.
+ */
+
+/**
+ * Placeholder for all chapters.
+ *
+ * @author せいうはん (Wesleysheng)
+ * @version 1.0.0, 2020-12-04 20:52
+ * @since 1.0.0, 2020-12-04 20:52
+ */
+package moe.yufan.study;
