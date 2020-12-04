a6f182b4aec41cd42e1bc48fba7fe5e6b6d0437b
syhily <syhily@gmail.com>
2020-12-04 23:45:06 +0800

    test: fix display name generator.

--- src/test/java/moe/yufan/study/chapter1/section1/CompareDoubleTest.java
+++ src/test/java/moe/yufan/study/chapter1/section1/CompareDoubleTest.java
@@ -1,7 +1,5 @@
 package moe.yufan.study.chapter1.section1;
 
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,7 +7,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 /**
  * @author せいうはん (Wesleysheng)
  */
-@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SuppressWarnings({"squid:S1192", "squid:S00100"})
 class CompareDoubleTest {
 
